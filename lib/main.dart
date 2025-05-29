977c54749f6d215a1db9ef8f4eb9b3bbdd79c407
Amal A M <vectorhero007@gmail.com>
2025-05-29 11:05:33 +0530

    stateful

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+// class DicePage extends StatelessWidget {
+//   // removed const from const DiscePage({super.key});
+//   DicePage({super.key});
+//   var DiceNum = 2;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Row(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Container(
+//             height: 150.0,
+//             width: 150.0,
+//             child: TextButton(
+//               style: TextButton.styleFrom(
+//                 foregroundColor: Colors.green.shade900,
+//               ),
+//               onPressed: () {
+//                 print('$DiceNum btn is pressed');
+//               },
+//               child: Image.asset('assets/images/dice$DiceNum.png'),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
 
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var diceNum = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -25,8 +61,13 @@ class DicePage extends StatelessWidget {
             height: 150.0,
             width: 150.0,
             child: TextButton(
-              onPressed: () {},
-              child: Image.asset('assets/images/dice1.png'),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.green.shade900,
+              ),
+              onPressed: () {
+                print('$diceNum btn is pressed');
+              },
+              child: Image.asset('assets/images/dice$diceNum.png'),
             ),
           ),
         ],
